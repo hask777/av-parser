6c2d18a21f9d9683ddbb5eace7ab28441a175b73
hask777 <haskellisp@gmail.com>
2021-04-29 19:20:44 +0300

    f

--- av.py
+++ av.py
@@ -1,45 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 base_url = 'https://cars.av.by/'
-
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-}
-
-r = requests.get('https://cars.av.by/acura')
-
-soup = BeautifulSoup(r.content, 'html.parser')
-carslist = soup.find_all('div', class_='listing-item')
 
 carslinks = {}
 
 finalcars = []
 
-for item in carslist:
+for x in range(1,5):
 
-    ''' Find car image '''
-    photo = item.find('div', class_='listing-item__photo')
-    image = photo.find('img')['data-src']
-
-    ''' Find car title '''
-    title = item.find('span', class_='link-text').text
-
-    ''' Find car link '''
-    params = item.find('div', class_='listing-item__params').text
-
-    ''' Find car link '''
-    link = item.find('a', href=True)['href']
- 
-    carslinks = {
-        'image': image,
-        'title': title,
-        'params': params,
-        'link': link
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
     }
 
-    finalcars.append(carslinks)
+    r = requests.get(f'https://cars.av.by/filter?brands[0][brand]=1444&price_currency=2&page={x}')
 
-print(finalcars[1])
+    soup = BeautifulSoup(r.content, 'html.parser')
+    carslist = soup.find_all('div', class_='listing-item')
+
+    
+
+    for item in carslist:
+
+        ''' Find car image '''
+        photo = item.find('div', class_='listing-item__photo')
+        image = photo.find('img')['data-src']
+
+        ''' Find car title '''
+        title = item.find('span', class_='link-text').text
+
+        ''' Find car link '''
+        params = item.find('div', class_='listing-item__params').text
+
+        ''' Find car link '''
+        link = item.find('a', href=True)['href']
+    
+        carslinks = {
+            'image': image,
+            'title': title,
+            'params': params,
+            'link': link
+        }
+
+        finalcars.append(carslinks)
+
+print(len(finalcars))
+
+
+cars = "cars.json"
+with open(cars, 'w') as f:
+    json.dump(finalcars, f, indent=4)
+
+print("file dumped")
     
    
